a43b962bb158f20947958210f9b807e7b6f304fb
ddoroshchuk <doroshukdashuly@mail.ru>
2018-08-16 22:07:37 +0300

    modified AuthorizationPage, created Tests

---
+++ src/main/java/Tests.java
@@ -0,0 +1,5 @@
+public class Tests {
+    public static void Main(String[] args){
+
+    }
+}
--- src/main/java/pages/AuthorizationPage.java
+++ src/main/java/pages/AuthorizationPage.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class AuthorizationPage {
     private WebDriver driver;
@@ -13,6 +14,17 @@ public class AuthorizationPage {
     @FindBy(xpath="//form[@id='login_form']//div[@class='form-group']//input[@name='passwd']")
     public WebElement passwordTF;
 
+    @FindBy(xpath="//form[@id='login_form']//div[@class='form-group row-padding-top']//button[@name='submitLogin']")
+    public WebElement logInBtn;
+
+    @FindBy(xpath="//input[@id='stay_logged_in']")
+    public WebElement rememberMeCheckbox;
+
+    public AuthorizationPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
     private void inputDataIntoTextField(WebElement element, String value){
         element.clear();
         element.sendKeys(value);
@@ -21,5 +33,13 @@ public class AuthorizationPage {
     public void signInToAccount(String email, String password){
         inputDataIntoTextField(emailTF, email);
         inputDataIntoTextField(passwordTF, password);
+        logInBtn.click();
+    }
+
+    public void signInToAccountAndRememberData(String email, String password){
+        inputDataIntoTextField(emailTF, email);
+        inputDataIntoTextField(passwordTF, password);
+        rememberMeCheckbox.click();
+        logInBtn.click();
     }
 }
